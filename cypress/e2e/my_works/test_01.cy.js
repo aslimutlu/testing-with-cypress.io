46799319c0009cf5184f98ce97ae19ee5ece193f
Aslı Mutlu <32126870+aslimutlu@users.noreply.github.com>
2023-12-10 02:29:16 +0300

    feat: create first test file

---
+++ cypress/e2e/my_works/test_01.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context("first test", () => {
+    // Her test boş bir sayfayla başlar. Bu nedenle testlerin yapılacağı sayfaya yönlendirme yapmak gerekir.
+    it("visit", () => {
+        cy.visit("https://newseasonn35.com/")
+    })
+
+    // Sayfadaki text kontrolü yapıldı.
+    it("text control",() => {
+        cy.get('body > div > div.page-wrapper.js-smooth-scroll.smooth-scroll > div.scroll-content > main > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-0bd9285.elementor-section-full_width.section_minus-mt-large.bg-dark.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div > div > div.container.section-services__inner > div > div:nth-child(1) > a > div.section-services__wrapper-button > div > div.link-arrow__label').should("have.text", "Daha Fazla Bilgi")
+    })
+})
